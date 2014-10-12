247e9a723be5624ac34c7453bcdba49d1fc477f0
catyabde <catia.abderhalden@gmail.com>
2014-10-12 09:17:56 +0200

    no message

--- src/com/masspeoplealfa/GcmBroadcastReceiver.java
+++ src/com/masspeoplealfa/GcmBroadcastReceiver.java
@@ -1,6 +1,9 @@
 package com.masspeoplealfa;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.TaskStackBuilder;
 import android.content.*;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
@@ -17,5 +20,19 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
         
         //Do something here with the notifications
         Log.i("notification","notification");
+        
+        Notification.Builder mBuilder =
+		        new Notification.Builder(context)
+        		.setSmallIcon(R.drawable.ic_launcher)
+		        .setContentTitle("Get the promo!")
+		        .setContentText("Go to Bar XX");
+		// Creates an explicit intent for an Activity in your app
+		//Intent resultIntent = new Intent(this, ResultActivity.class);
+
+//		mBuilder.setContentIntent(resultPendingIntent);
+		NotificationManager mNotificationManager =
+		    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		// mId allows you to update the notification later on.
+		mNotificationManager.notify(999, mBuilder.build());
     }
 }
